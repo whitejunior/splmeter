6aa8b89f7296178cbb0ab2a1309967f3c477f593
hashir.mail@gmail.com <hashir.mail@gmail.com@5efd17c2-5c06-11de-8888-c79ed7c8b0c3>
2009-09-12 09:28:23 +0000

    version 1.5 changes. Bug corrections after running on google phone.

--- trunk/1.0/spl/src/com/android/spl/Recorder.java
+++ trunk/1.0/spl/src/com/android/spl/Recorder.java
@@ -11,12 +11,6 @@
 
 package com.android.spl;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.math.BigDecimal;
 
 import android.media.AudioFormat;
@@ -25,249 +19,210 @@ import android.media.MediaRecorder;
 import android.os.Handler;
 import android.os.Message;
 
-public class Recorder implements Runnable {
+public class Recorder implements Runnable
+{
+ 
+    private static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int MY_MSG = 1;
+    private int channelConfiguration;;
+
     private int frequency;
-    private int channelConfiguration;
-    private File fileName;
-    private volatile boolean isRecording = false;;
+    Handler handle;
+    private volatile boolean isRecording = false;
 
     short[] tempBuffer;
-    Handler handle;
-	private static final  int MY_MSG = 1;
-	
-
-    // Changing the sample resolution changes sample type. byte vs. short.
-    private static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
 
     /**
-     * Handler is passed to pass messages to main screen
-     * Recording is done 8000Hz MONO 16 bit
+     * Handler is passed to pass messages to main screen Recording is done
+     * 8000Hz MONO 16 bit
      */
-    public Recorder(Handler h) {
-         super();
-         this.setFrequency(8000);
-         this.setChannelConfiguration(AudioFormat.CHANNEL_CONFIGURATION_MONO);
-         this.handle = h;
-    }
-
-    /* Recording THREAD */
-    public void run() {
-         // Wait until we're recording...
-    	 AudioRecord recordInstance = null;
-    	 BufferedOutputStream bufferedStreamInstance = null;
-    	   
-    	     
-	         // We're important...
-	         android.os.Process
-	                   .setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-	
-	         // Allocate Recorder and Start Recording...
-	         int bufferRead = 0;
-	         //int bufferSize = AudioRecord.getMinBufferSize(this.getFrequency(),
-	         //          this.getChannelConfiguration(), this.getAudioEncoding());
-	         int bufferSize = 4096;//2048;
-	         
-	         recordInstance = new AudioRecord(
-	                   MediaRecorder.AudioSource.MIC, this.getFrequency(), this
-	                             .getChannelConfiguration(), this.getAudioEncoding(),
-	                   bufferSize);
-	         
-	         tempBuffer = new short[bufferSize];
-	         recordInstance.startRecording();
-	   
-	      // Continue till STOP button is pressed.   
-	      while (this.isRecording) {
-	    	 
-	    	 // Re Create file each time to save space. 
-	    	 if (fileName.exists()) {
-	              fileName.delete();
-	         }
-	         try {
-	              fileName.createNewFile();
-	         } catch (IOException e) {
-	              throw new IllegalStateException("Cannot create file: " + fileName.toString());
-	         }
-	         
-               
-	         // Open output stream...
-	         if (this.fileName == null) {
-	              throw new IllegalStateException("fileName is null");
-	         }
-	     
-	        
-	         try {
-	              bufferedStreamInstance = new BufferedOutputStream(
-	                        new FileOutputStream(this.fileName));
-	         } catch (FileNotFoundException e) {
-	              throw new IllegalStateException("Cannot Open File", e);
-	         }
-	         DataOutputStream dataOutputStreamInstance =
-	              new DataOutputStream(bufferedStreamInstance);
-	         
-	         
-	         bufferRead = recordInstance.read(tempBuffer, 0, bufferSize);
-	         
-              if (bufferRead == AudioRecord.ERROR_INVALID_OPERATION) {
-                   throw new IllegalStateException(
-                             "read() returned AudioRecord.ERROR_INVALID_OPERATION");
-              } else if (bufferRead == AudioRecord.ERROR_BAD_VALUE) {
-                   throw new IllegalStateException(
-                             "read() returned AudioRecord.ERROR_BAD_VALUE");
-              } else if (bufferRead == AudioRecord.ERROR_INVALID_OPERATION) {
-                   throw new IllegalStateException(
-                             "read() returned AudioRecord.ERROR_INVALID_OPERATION");
-              }
-              try {
-                   for (int idxBuffer = 0; idxBuffer < bufferRead; ++idxBuffer) {
-                        dataOutputStreamInstance.writeShort(tempBuffer[idxBuffer]);
-                   }
-                  measure(bufferSize);  // calucalte SPL
-              } catch (IOException e) {
-                   throw new IllegalStateException(
-                        "dataOutputStreamInstance.writeShort(curVal)");
-              }
-              
-          }
-       
-         // STOP BUTTON WAS PRESSED.
-         // Close resources...
-         recordInstance.stop();
-         try {
-              bufferedStreamInstance.close();
-         } catch (IOException e) {
-              throw new IllegalStateException("Cannot close buffered writer.");
-         }
-    }
-    
-    /**
-     * Calculate SPL
-     *  P = square root ( 2*Z*I ) - > Pressure
-     *  Z = Acoustic Impedance = 406.2 for air at 30 degree celsius
-     *  I = Intensity = 2*Z*pi square*frequency square*Amplitude square
-     *  @param bsize - the size of FFT required.
-     */
-    public void measure(int bsize){
-    	int i = 0;
-    	double frequency = 0;
-    	double amplitude = 0;
-    	double max = 0.0;
-    	int max_index = 0;
-    	double w = 0.0;
-    	
-    	 
-    	double Z   = 406.2;
-    	double I   = 0.0;
-    	double P   = 0.0;
-    	double P0  = 2*0.00001;  //is constant
-    	double Istar = 0.0;  // SPL
-    	
-    	Complex[] x = new Complex[bsize];
-    	
-    	for( i = 0; i < bsize; i++ ){
-    		x[i] = new Complex(tempBuffer[i],0);
-    	}
-    	
-    	Complex[] xf = new Complex[bsize];
-    	xf = FFT.fft(x);
-    	
-    	for( i = 0; i < bsize/2; i++ ){
-    		w = xf[i].abs();
-    		if ( w > max ){
-    			max_index = i;
-    			max = w;
-    		}
-    	}
-    	// Frequency and Amp of fundamental frequency
-    	frequency = max_index;
-    	amplitude = xf[max_index].abs()*2/bsize;
-    	    	
-    	I  = Z*2*2*Math.PI*Math.PI*frequency*frequency*amplitude*amplitude;
-    	P  = Math.sqrt(Z*I);
-        if ( P != 0 ) 
-        	Istar = round(20*Math.log10(P/P0)/10,3);  // divide by 10 to correct the calculation
-        
-    	
-    	Message msg = handle.obtainMessage(MY_MSG,"\n\n"+Istar+ " db SPL");
-		handle.sendMessage(msg);
-    }
-    
-    /**
-     * Utility Function to round a double value
-     * @param d  - The decimal value
-     * @param decimalPlace - how many places required
-     * @return double - the rounded value
-     */
-    public double round(double d, int decimalPlace){
-        // see the Javadoc about why we use a String in the constructor
-        // http://java.sun.com/j2se/1.5.0/docs/api/java/math/BigDecimal.html#BigDecimal(double)
-        BigDecimal bd = new BigDecimal(Double.toString(d));
-        bd = bd.setScale(decimalPlace,BigDecimal.ROUND_HALF_UP);
-        return bd.doubleValue();
-      }
-
-    public void setFileName(File fileName) {
-         this.fileName = fileName;
-    }
-
-    public File getFileName() {
-         return fileName;
+    public Recorder(Handler h)
+    {
+        super();
+        this.setFrequency(8000);
+        this.setChannelConfiguration(AudioFormat.CHANNEL_CONFIGURATION_MONO);
+        this.handle = h;
     }
 
     /**
-     * @param isRecording
-     *            the isRecording to set
+     * @return the audioEncoding
      */
-    public void setRecording(boolean isRecording) {
-        
-              this.isRecording = isRecording;
-     
+    public int getAudioEncoding()
+    {
+        return audioEncoding;
     }
 
     /**
-     * @return the isRecording
+     * @return the channelConfiguration
      */
-    public boolean isRecording() {
-        
-              return isRecording;
-         
-    }
-
-    /**
-     * @param frequency
-     *            the frequency to set
-     */
-    public void setFrequency(int frequency) {
-         this.frequency = frequency;
+    public int getChannelConfiguration()
+    {
+        return channelConfiguration;
     }
 
     /**
      * @return the frequency
      */
-    public int getFrequency() {
-         return frequency;
+    public int getFrequency()
+    {
+        return frequency;
+    }
+
+    
+    /**
+     * @return the isRecording
+     */
+    public boolean isRecording()
+    {
+
+        return isRecording;
+
+    }
+
+    /**
+     * Calculate SPL P = square root ( 2*Z*I ) - > Pressure Z = Acoustic
+     * Impedance = 406.2 for air at 30 degree celsius I = Intensity = 2*Z*pi
+     * square*frequency square*Amplitude square
+     * 
+     * @param bsize
+     *            - the size of FFT required.
+     */
+    public double measure(int bsize)
+    {
+        int i = 0;
+        double frequency = 0;
+        double amplitude = 0;
+        double max = 0.0;
+        int max_index = 0;
+        double w = 0.0;
+
+        double Z = 406.2;
+        double I = 0.0;
+        double P = 0.0;
+        double P0 = 2 * 0.00001; // is constant
+        double Istar = 0.0; // SPL
+
+        Complex[] x = new Complex[bsize];
+
+        for (i = 0; i < bsize; i++)
+        {
+            x[i] = new Complex(tempBuffer[i], 0);
+        }
+
+        Complex[] xf = new Complex[bsize];
+        xf = FFT.fft(x);
+
+        for (i = 1; i < bsize / 2; i++)
+        {
+            w = xf[i].abs();
+            if (w > max)
+            {
+                max_index = i;
+                max = w;
+            }
+        }
+        // Frequency and Amp of fundamental frequency
+        frequency = max_index * bsize;
+        amplitude = max * 2 / bsize;
+
+        I = Z * 2 * 2 * Math.PI * Math.PI * frequency * frequency * amplitude
+                * amplitude;
+        P = Math.sqrt(Z * I);
+        if (P != 0)
+            Istar = round(20 * Math.log10(P / P0) / 10, 3); // divide by 10 to
+                                                            // correct the
+                                                            // calculation
+
+        Message msg = handle.obtainMessage(MY_MSG, "\n\nFrequency = " + frequency + " Hz\n"
+                + Istar + " db SPL");
+        handle.sendMessage(msg);
+        return Istar;
+
+    }
+
+    /**
+     * Utility Function to round a double value
+     * 
+     * @param d
+     *            - The decimal value
+     * @param decimalPlace
+     *            - how many places required
+     * @return double - the rounded value
+     */
+    public double round(double d, int decimalPlace)
+    {
+        // see the Javadoc about why we use a String in the constructor
+        // http://java.sun.com/j2se/1.5.0/docs/api/java/math/BigDecimal.html#BigDecimal(double)
+        BigDecimal bd = new BigDecimal(Double.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.doubleValue();
+    }
+
+    /* Recording THREAD */
+    public void run()
+    {
+
+        AudioRecord recordInstance = null;
+
+        // We're important...
+        android.os.Process
+                .setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+
+        short bufferSize = 4096;// 2048;
+
+        recordInstance = new AudioRecord(MediaRecorder.AudioSource.MIC, this
+                .getFrequency(), this.getChannelConfiguration(), this
+                .getAudioEncoding(), bufferSize);
+
+        tempBuffer = new short[bufferSize];
+        recordInstance.startRecording();
+        // Continue till STOP button is pressed.
+
+        while (this.isRecording)
+        {
+            double splValue = 0.0;
+
+            for (int i = 0; i < tempBuffer.length; i++)
+            {
+                tempBuffer[i] = 0;
+            }
+
+            recordInstance.read(tempBuffer, 0, bufferSize);
+            splValue = measure(bufferSize); // calucalte SPL
+        }
+        // STOP BUTTON WAS PRESSED.
+        // Close resources...
+        recordInstance.stop();
+
     }
 
     /**
      * @param channelConfiguration
      *            the channelConfiguration to set
      */
-    public void setChannelConfiguration(int channelConfiguration) {
-         this.channelConfiguration = channelConfiguration;
+    public void setChannelConfiguration(int channelConfiguration)
+    {
+        this.channelConfiguration = channelConfiguration;
     }
 
     /**
-     * @return the channelConfiguration
+     * @param frequency
+     *            the frequency to set
      */
-    public int getChannelConfiguration() {
-         return channelConfiguration;
+    public void setFrequency(int frequency)
+    {
+        this.frequency = frequency;
     }
 
     /**
-     * @return the audioEncoding
+     * @param isRecording
+     *            the isRecording to set
      */
-    public int getAudioEncoding() {
-         return audioEncoding;
+    public void setRecording(boolean isRecording)
+    {
+
+        this.isRecording = isRecording;
+
     }
 
-
-} 
+}
--- trunk/1.0/spl/src/com/android/spl/SPL.java
+++ trunk/1.0/spl/src/com/android/spl/SPL.java
@@ -7,11 +7,8 @@
 
 package com.android.spl;
 
-import java.io.File;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -19,96 +16,96 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class SPL extends Activity {
+public class SPL extends Activity
+{
+    private static final int MY_MSG = 1;
     /** Called when the activity is first created. */
-	String filename;
-	private Button start_button;
-	private Button stop_button;
-	Recorder recorderInstance;
-	Thread th;
-	private static final  int MY_MSG = 1;
-	TextView tv;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        start_button = (Button) findViewById(R.id.Button01);      // start button
-        start_button.setOnClickListener(start_button_handle);
-        stop_button = (Button) findViewById(R.id.Button02);       // stop button
-        stop_button.setOnClickListener(stop_button_handle);
-        tv = (TextView) findViewById(R.id.TextView01);            // text area for displaying result
-        tv.setTextSize(1, 30);
-        tv.setText(" db SPL");
-        
 
-        start_button.setEnabled(false);
-        stop_button.setEnabled(true);
-        
-        start_scan();  // start the measuring automatically when application starts
-       
-    }
-    
-    private void start_scan(){
-    	
-    	recorderInstance = new Recorder(mhandle);
-    	recorderInstance.setRecording(true);
-        th = new Thread(recorderInstance);
-        // writing to /sdcard/test.raw. Rewritten each time 
-        recorderInstance.setFileName(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.raw"));
-        th.start();
-        start_button.setEnabled(false);  // start automatically on start
-        stop_button.setEnabled(true);
-        
-    }
-    
+    String filename;
+    /* Handler for displaying messages from recording thread */
+    public Handler mhandle = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            switch (msg.what)
+            {
+            case MY_MSG:
+                tv.setText("" + msg.obj);
+                break;
+            default:
+                super.handleMessage(msg);
+                break;
+            }
+        }
+
+    };
+    Recorder recorderInstance;
+    private Button start_button;
     /* Start Button Handler */
     private OnClickListener start_button_handle = new OnClickListener()
     {
 
-		@Override
-		public void onClick(View arg0) {
-			start_scan();
+        public void onClick(View arg0)
+        {
+            start_scan();
 
-	       
-		}
+        }
     };
-    
+    private Button stop_button;
+
     /* Stop Button Handler */
     private OnClickListener stop_button_handle = new OnClickListener()
     {
 
-		@Override
-		public void onClick(View v) {
-			
-			
-			recorderInstance.setRecording(false);		
-	        //th.join();			
-	        start_button.setEnabled(true);
-	        stop_button.setEnabled(false); 
-	        
-			
-		}
-    };
-    
-    /* Handler for displaying messages from recording thread */     
-    public Handler mhandle = new Handler()
-     {
-  	   @Override
-      	   public void handleMessage(Message msg) {
-  	   		switch(msg.what)
-      			{
-	       		case MY_MSG:
-	      			tv.setText(""+msg.obj);
-	       			break;
-	       		default:
-	       			super.handleMessage(msg);
-	       		    break;
-      			}
-  	   		}
-  	   
-     };
+        public void onClick(View v)
+        {
 
-    
+            recorderInstance.setRecording(false);
+            // th.join();
+            start_button.setEnabled(true);
+            stop_button.setEnabled(false);
+
+        }
+    };
+
+    Thread th;
+
+    TextView tv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        start_button = (Button) findViewById(R.id.Button01); // start button
+        start_button.setOnClickListener(start_button_handle);
+        stop_button = (Button) findViewById(R.id.Button02); // stop button
+        stop_button.setOnClickListener(stop_button_handle);
+        tv = (TextView) findViewById(R.id.TextView01); // text area for
+                                                       // displaying result
+        tv.setTextSize(1, 30);
+        tv.setText(" db SPL");
+
+        start_button.setEnabled(false);
+        stop_button.setEnabled(true);
+
+        start_scan(); // start the measuring automatically when application
+                      // starts
+
+    }
+
+    private void start_scan()
+    {
+
+        recorderInstance = new Recorder(mhandle);
+        recorderInstance.setRecording(true);
+        th = new Thread(recorderInstance);
+        th.start();
+        start_button.setEnabled(false); // start automatically on start
+        stop_button.setEnabled(true);
+
+    }
+
 }
